17d2782486a33924211ee43fa37f8988fcdb5dec
Jack Smith <B00308927@studentmail.uws.ac.uk>
2019-03-24 18:56:36 +0000

    Attempted to add double precision matmul but the reduction needs some work. May need to rethink the specific use of intrinsic here.

--- matmul-assignment.cpp
+++ matmul-assignment.cpp
@@ -103,20 +103,7 @@ float SIMD_VReduce(__m128 v, const size_t size)
 	return sum;
 }
 
-double SIMD_VReduce(__m256d v, const size_t size)
-{
-	double sum;
-	__m256d vsum = _mm256_set1_pd(0.0f);
-	for (std::size_t i = 0; i < size; i += size) {
-		vsum = _mm256_add_pd(vsum, v);
-	}
-	vsum = _mm256_hadd_pd(vsum, vsum);
-	vsum = _mm256_hadd_pd(vsum, vsum);
-	_mm256_store_sd(&sum, vsum);
-	return sum;
-}
-
-void SIMD_MatMul(mat &mres, const mat &m1, const mat &m2, const int matrix_dimension)
+void SIMD_MatMul(mat &mres, const mat &m1, const mat &m2)
 {
 	__m128 row, column, dotProduct, newRow;
 	float reducedFloat;
@@ -127,14 +114,14 @@ void SIMD_MatMul(mat &mres, const mat &m1, const mat &m2, const int matrix_dimen
 	//std::vector<*float> columnSections;
 	for (int i = 0; i < mres.sz; i++) 
 	{
-		row = _mm_load_ps(&m1.data[i*mres.sz]); // 1. Get Row Values.
+		row = _mm_load_ps(&m1.data[i*mres.sz]);							// 1. Get Row Values.
 		// What intrinsics should be used when the rows are bigger than 4? __m256, etc? Would this need deduced
 		// at runtime or simply passed in as a type to be used in the method?
 		for (int j = 0; j < mres.sz; j++)
 		{
 			for (int y = 0; y < mres.sz; y++)
 			{
-				columnSections[y] = m2.data[y *mres.sz + j]; // 2. Get column data
+				columnSections[y] = m2.data[y *mres.sz + j];			// 2. Get column data
 
 				// Use add_ps here instead to load via index?
 			}
@@ -145,66 +132,81 @@ void SIMD_MatMul(mat &mres, const mat &m1, const mat &m2, const int matrix_dimen
 			/*_mm_dp_ps(row, column, What is the third parameter here?);
 			https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=_mm_dp_ps&expand=2160
 			*/
-			dotProduct = _mm_mul_ps(row, column); // 4. Compute dot product of row and column
-			reducedFloat = SIMD_VReduce(dotProduct, matrix_dimension); // 5. Reduce to a single float
-			newRowFloats[j] = reducedFloat; // 6. Store float in appropriate index.
-		}
+			dotProduct = _mm_mul_ps(row, column);						// 4. Compute dot product of row and column
+			reducedFloat = SIMD_VReduce(dotProduct, mres.sz);			// 5. Reduce to a single float
+			newRowFloats[j] = reducedFloat;								 // 6. Store float in appropriate index.
+		} // Repeat for every column
 		newRow = _mm_load_ps(&newRowFloats[0]);
-		_mm_store_ps(&mres.data[i*mres.sz], newRow); // 7. Add new row into Matrix 
-	}
+		_mm_store_ps(&mres.data[i*mres.sz], newRow);					// 7. Add new row into Matrix 
+	} // Repeat for every row
 }
 
-void SIMD_MatMul(matd &mres, const matd &m1, const matd &m2, const int matrix_dimension)
-{
-	__m256d row, column, dotProduct, newRow;
-	double reducedDouble;
-	std::vector<double> columnSections;
-	columnSections.resize(mres.sz);
-	std::vector<double> newRowDoubles;
-	newRowDoubles.resize(mres.sz);
-	for (int i = 0; i < mres.sz; i++)
-	{
-		row = _mm256_load_pd(&m1.data[i*mres.sz]); // 1. Get Row Values.
-		for (int j = 0; j < mres.sz; j++)
-		{
-			for (int y = 0; y < mres.sz; y++)
-			{
-				columnSections[y] = m2.data[y *mres.sz + j]; // 2. Get column data
-			}
-			column = _mm256_load_pd(&columnSections[0]); // 3. Place column values into an __m128
-			dotProduct = _mm256_mul_pd(row, column); // 4. Compute dot product of row and column
-			reducedDouble = SIMD_VReduce(dotProduct, matrix_dimension); // 5. Reduce to a single float
-			newRowDoubles[j] = reducedDouble; // 6. Store float in appropriate index.
-		}
-		newRow = _mm256_load_pd(&newRowDoubles[0]);
-		_mm256_store_pd(&mres.data[i*mres.sz], newRow); // 7. Add new row into Matrix 
-	}
-}
+//double SIMD_VReduce(__m256d v, const size_t size)
+//{
+//	double sum;
+//	__m256d vsum = _mm256_set1_pd(0.0f);
+//	for (std::size_t i = 0; i < size; i += size) {
+//		vsum = _mm256_add_pd(vsum, v);
+//	}
+//	vsum = _mm256_hadd_pd(vsum, vsum);
+//	vsum = _mm256_hadd_pd(vsum, vsum);
+//	_mm_store_sd(&sum, vsum);
+//	/* Not sure this is right. _mm256_store_pd will store the whole intrinsic, but _mm_store_ss will Store the lowest 32 bit float of a into memory.
+//	 What is the double precision equivilent?
+//	 */
+//	return sum;
+//}
+//void SIMD_MatMul(matd &mres, const matd &m1, const matd &m2, const int matrix_dimension)
+//{
+//	__m256d row, column, dotProduct, newRow;
+//	double reducedDouble;
+//	std::vector<double> columnSections;
+//	columnSections.resize(mres.sz);
+//	std::vector<double> newRowDoubles;
+//	newRowDoubles.resize(mres.sz);
+//	for (int i = 0; i < mres.sz; i++)
+//	{
+//		row = _mm256_load_pd(&m1.data[i*mres.sz]); // 1. Get Row Values.
+//		for (int j = 0; j < mres.sz; j++)
+//		{
+//			for (int y = 0; y < mres.sz; y++)
+//			{
+//				columnSections[y] = m2.data[y *mres.sz + j]; // 2. Get column data
+//			}
+//			column = _mm256_load_pd(&columnSections[0]); // 3. Place column values into an __m128
+//			dotProduct = _mm256_mul_pd(row, column); // 4. Compute dot product of row and column
+//			reducedDouble = SIMD_VReduce(dotProduct, matrix_dimension); // 5. Reduce to a single float
+//			newRowDoubles[j] = reducedDouble; // 6. Store float in appropriate index.
+//		}
+//		newRow = _mm256_load_pd(&newRowDoubles[0]);
+//		_mm256_store_pd(&mres.data[i*mres.sz], newRow); // 7. Add new row into Matrix 
+//	}
+//}
 
 // Fastmath /pf:fast flag
 // Look for custom memory allocator in slides to align memeory appropriately
 
 int main(int argc, char *argv[])
 {
-  std::size_t size = SZ * SZ * sizeof(float);
-  std::size_t space = size + 16;
-  void *p = std::malloc(space);
+  //std::size_t size = SZ * SZ * sizeof(float);
+  //std::size_t space = size + 16;
+  //void *p = std::malloc(space);
   
-  void *pp = std::align(16, size, p, space);
+  //void *pp = std::align(16, size, p, space);
   //std::aligned_alloc(16, SZ*SZ*sizeof(float));
-  alignas(sizeof(__m128)) mat mres{ new float[SZ*SZ],SZ},mres2{ new float[SZ*SZ],SZ },m{new float[SZ*SZ],SZ}, mres4{ new float[SZ*SZ], SZ }, id{new float[SZ*SZ],SZ};
-  alignas(sizeof(__m256d)) matd mres3 { new double[SZ*SZ], SZ }, /*mres4{ new double[SZ*SZ], SZ },*/ md{ new double[SZ*SZ],SZ }, idd{ new double[SZ*SZ],SZ };
+  alignas(sizeof(__m128)) mat mres{ new float[SZ*SZ],SZ},mres2{ new float[SZ*SZ],SZ },m{new float[SZ*SZ],SZ}, id{new float[SZ*SZ],SZ};
+  alignas(sizeof(__m256d)) matd mres3 { new double[SZ*SZ], SZ }, md{ new double[SZ*SZ],SZ }, idd{ new double[SZ*SZ],SZ };
   using namespace std::chrono;
   using tp_t = time_point<high_resolution_clock>;
-  tp_t t1, t2, t3, t4, t5, t6, t7, t8;
+  tp_t t1, t2, t7, t8, t9, t10;
 
   std::cout << "Each " << SZ << 'x' << SZ;
   std::cout << " matrix is " << sizeof(float)*SZ*SZ << " bytes.\n\n";
 
   init_mat(m);
-  //init_mat(md);
+  init_mat(md);
   identity_mat(id);
-  //identity_mat(idd);
+  identity_mat(idd);
 
   t1 = high_resolution_clock::now();
   matmul(mres,m,id);
@@ -226,7 +228,7 @@ int main(int argc, char *argv[])
   std::cout << "Serial Multiplication took " << d << ' ' << "microseconds.\n\n";
 
   t7 = high_resolution_clock::now();
-  SIMD_MatMul(mres2, m, id, SZ); // It's right but only by accident
+  SIMD_MatMul(mres2, m, id);
   t8 = high_resolution_clock::now();
 
   /*
@@ -255,19 +257,40 @@ int main(int argc, char *argv[])
   const auto d4 = duration_cast<microseconds>(t8 - t7).count();
   std::cout << "SIMD Single Precision Multiplication took " << d4 << ' ' << "microseconds.\n";
 
+  //t9 = high_resolution_clock::now();
+  //SIMD_MatMul(mres3, md, idd, SZ); // It's right but only by accident
+  //t10 = high_resolution_clock::now();
+
+  //std::cout
+	 // << "//////////////////////////////////////////////\n"
+	 // << "//// SIMD Double Precision Multiplication ////\n"
+	 // << "//////////////////////////////////////////////\n"
+	 // << std::endl;
+
+  //std::cout << "Initial Matrix" << "\n\n";
+  //print_mat(md);
+  //std::cout << "Identity Matrix" << "\n\n";
+  //print_mat(idd);
+  //std::cout << "Resultant Matrix" << "\n\n";
+  //print_mat(mres3);
+
+  //const auto d5 = duration_cast<microseconds>(t10 - t9).count();
+  //std::cout << "SIMD Double Precision Multiplication took " << d5 << ' ' << "microseconds.\n";
+
   const bool correct = mres == m;
   const bool correct2 = mres2 == m;
+ // const bool correct3 = mres3 == md;
+
  // const bool correct3 = mres3 == md;
  // const bool correct4 = mres4 == m;
 
   delete [] mres.data;
   delete [] mres2.data;
-  //delete [] mres3.data;
-  //delete [] mres4.data;
+  delete [] mres3.data;
   delete [] m.data;
-  //delete [] md.data;
+  delete [] md.data;
   delete [] id.data;
- // delete [] idd.data;
+  delete [] idd.data;
 
 #ifdef _WIN32
   system("pause");
